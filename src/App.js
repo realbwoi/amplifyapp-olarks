0be6276a0751f41e9ec94c768c6d2424e82d5cac
realbwoi <olarks@gmail.com>
2021-08-31 08:21:41 -0700

    Adding more content

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Hello from V2</h1>
+        <p>Adding content...</p>
       </header>
     </div>
   );
